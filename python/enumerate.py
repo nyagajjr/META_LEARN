33f6ade27538784ba476ded8cf5fe301661745b2
nyagajr <nyagaben123@gmail.com>
2024-08-28 22:45:43 +0200

    Refreshing on Python enumerate

---
+++ python/enumerate.py
@@ -0,0 +1,6 @@
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+
+# Use enumerate to get both the index and the value
+for idx, item in enumerate(num_list):
+    if item == 36:
+        print('Number found at position:', idx)

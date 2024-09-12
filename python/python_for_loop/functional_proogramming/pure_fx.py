c4147f36c91fa7d937588262ce10f63926ea6c9a
nyagajr <nyagaben123@gmail.com>
2024-09-12 12:56:34 +0200

    Functional programming pure functions

---
+++ python/python_for_loop/functional_proogramming/pure_fx.py
@@ -0,0 +1,11 @@
+# Pure functions are functions that don't alter the data outside its scope. e.g
+
+my_list = [1,2,3,4,5]
+
+def pure_fx(lst, item):
+    nl = lst.copy()
+    nl.append(item)
+    return nl
+
+print(pure_fx(my_list, 6)) # Not accessing the outside data from my_list
+print(my_list) #Maintains the original data
